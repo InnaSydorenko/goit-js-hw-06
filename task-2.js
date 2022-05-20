8ddea2c346a698ab69d374405ac564c0520f560c
InnaSydorenko <88403491+InnaSydorenko@users.noreply.github.com>
2022-05-20 13:26:36 +0200

    task-2

---
+++ task-2.js
@@ -0,0 +1,25 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+
+const ulContainer = document.querySelector('ul');
+
+
+
+
+const elements = ingredients.map(ingredient => {
+    const liEl = document.createElement('li');
+
+    liEl.textContent = ingredient;
+    liEl.classList.add('item')
+    return liEl;
+})
+
+   ulContainer.append(...elements)
+  console.log(ulContainer);
